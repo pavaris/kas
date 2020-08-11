3ac7041be7491f18c68b6b6e41809e9bebcc26f2
Pavaris Ketavanan <pavarisket@gmail.com>
2020-08-10 22:23:52 -0400

    donate page

--- wp-content/themes/kas_2020/page-donate.php
+++ wp-content/themes/kas_2020/page-donate.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
- 	<div id="primary" class="content-area">
+ 	<div id="primary" class="content-area page-donate">
  		<main id="main" class="site-main">
 		<?php
  					while ( have_posts() ) :
@@ -13,17 +13,25 @@
 				</div>
 			</div>
 
-	
- 						<section class='page-content'>
-							 <div class="content-margins">
-								 <?php the_content(); ?>
-							 </div>
- 						</section>
- 			
 
-		<?php
- 					endwhile; // End of the loop.
- 					?>
+			<section class='page-content'>
+					<div class="content-margins">
+						<?php the_content(); ?>
+					</div>
+			</section>
+			
+			<?php $image = get_field('image'); ?>
+			<?php $text = get_field('text'); ?>
+			<?php if($image){
+?> 
+<div class="donate-footer-image">
+	<?php echo wp_get_attachment_image($image['id'], 'large'); ?>	
+	<h5><?php echo $text; ?></h5>
+</div>
+<?php 
+			} ?>
+			
+			<?php endwhile; // End of the loop. ?>
 		</main>
 	</div>
 
