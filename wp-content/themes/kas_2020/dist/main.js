4f5eeb51ca647c7439b5f4e0b205fd5a6afc646a
Pavaris Ketavanan <pavarisket@gmail.com>
2020-08-31 20:51:10 -0400

    remove mrs eaves

--- wp-content/themes/kas_2020/dist/main.js
+++ wp-content/themes/kas_2020/dist/main.js
@@ -74,7 +74,7 @@ __webpack_require__(2);
 /* 1 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n\tfont-family: $mrsEaves;\n             ^\n      Undefined variable: \"$mrsEaves\".\n      in /Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/sass/global/_archive.scss (line 4, column 15)\n    at /Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/webpack/lib/NormalModule.js:195:19\n    at /Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.callback (/Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/neo-async/async.js:8067:18)\n    at options.error (/Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 /* 2 */
