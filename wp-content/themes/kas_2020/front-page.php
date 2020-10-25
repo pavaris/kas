761e7c92da2d07584c805a56640bf73d7df2c218
Pavaris Ketavanan <pavarisket@gmail.com>
2020-10-24 22:52:59 -0400

    video stuff

--- wp-content/themes/kas_2020/front-page.php
+++ wp-content/themes/kas_2020/front-page.php
@@ -91,8 +91,8 @@ get_header();
                               </div>
                             </a>
                         
-                            <div class="home-legacy-feed ">
-                              <div class="content-margins wide">
+                            <div class="home-legacy-feed">
+                              <div class="home-legacy-feed-inside">
                                 <div class="legacy-arrow legacy-arrow-left">
                                   <button>
                                     <img src="<?php echo get_template_directory_uri(  ); ?>/img/chevron.svg" alt="arrow">
