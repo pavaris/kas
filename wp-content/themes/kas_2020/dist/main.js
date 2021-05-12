2e8a16acd82a198df3e82ae5164379bdd3e3bd9d
pavaris <pavarisket@gmail.com>
2021-05-11 21:53:42 -0400

    recompile

--- wp-content/themes/kas_2020/dist/main.js
+++ wp-content/themes/kas_2020/dist/main.js
@@ -74,7 +74,7 @@ __webpack_require__(2);
 /* 1 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n\ttransition-duration: ;\n                   ^\n      Style declaration must contain a value\n      in /Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/sass/global/_single.scss (line 158, column 21)\n    at /Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/webpack/lib/NormalModule.js:195:19\n    at /Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.callback (/Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/neo-async/async.js:8067:18)\n    at options.error (/Users/pavaris/dev/kas_new/wp-content/themes/kas_2020/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 /* 2 */
